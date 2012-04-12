8ef6817d09d82337374a8f375ec852fc6bbbfa3a
Helzibah <helen.m.gray@gmail.com>
2012-04-12 22:37:34 +0100

    Added persistent mongodb backend for abbreviations.

--- csbot/plugins/abbreviations.py
+++ csbot/plugins/abbreviations.py
@@ -1,4 +1,4 @@
-from csbot.core import Plugin, command
+from csbot.core import Plugin, PluginFeatures
 
 # Abbreviations plugin
 # !abbr.add
@@ -21,97 +21,105 @@ from csbot.core import Plugin, command
 # poss some kind of mass add option if scrapable from anywhere
 
 # TODO:
-# - make persistent, ideally with a better backend than just strings, e.g. pickle
 # - auto expansion!
 
 class Abbreviations(Plugin):
+    features = PluginFeatures()
+
     DATA = 'dictionary'
+    # TODO: change usage text to use user input?
     USAGE_ADD = "Usage: !abbr.add TLA \"Three Letter Acronym\""
     USAGE_EDIT = "Usage: !abbr.edit TLA \"Three Letter Abbreviation\""
     USAGE_ABBR = "Usage: !abbr TLA"
     USAGE_REM = "Usage: !abbr.rem TLA"
 
-    abbrs = {}
-
     def setup(self):
-        # TODO: store persistent dictionary in db
         pass
-#        try:
-#            abbrs = self.get(self.DATA)
-#        except:
-#            self.set(self.DATA, {})
 
-    @command('abbr.add')
+    def teardown(self):
+        pass
+
+    # TODO:
+    # use remaining event data rather than event data [1]?
+    #   > abbr.add TLA Three Letter Acronym
+    # or: keep as is; allows more expansion
+    #   e.g. general 'facts' plugin with short, expansion + link
+    @features.command('abbr.add')
     def add_command(self, event):
         if len(event.data) < 2:
             if len(event.data) < 1:
                 event.error("I can't add an empty abbreviation!")
             else:
-                event.error("You need to define the expansion for '%s'" % event.data[0])
+                event.error("You need to define the expansion for '{}'".format(event.data[0]))
 
             event.reply(self.USAGE_ADD, True)
             return
 
-        #abbrs = self.get(self.DATA)
         abbreviation = event.data[0]
         expansion = event.data[1]
+        current = self.db.abbrs.find_one({"abbreviation": abbreviation})
 
-        if abbreviation in self.abbrs:
-            event.error("I already have an expansion, '%s' = '%s'. Try the edit command." % (abbreviation, self.abbrs[abbreviation]))
+        if current is not None:
+            event.error("I already have an expansion, '{}' = '{}'. Try the abbr.edit command.".format(abbreviation, current["expansion"]))
             event.reply(self.USAGE_EDIT, True)
         else:
-            self.abbrs[abbreviation] = event.data[1]
-#            self.set(self.DATA, self.abbrs)
-            event.reply("Added '%s' = '%s'." % (abbreviation, expansion))
+            self.db.abbrs.insert({"abbreviation": abbreviation, "expansion": expansion})
+            event.reply("Added '{}' = '{}'.".format(abbreviation, expansion))
 
 
-    @command('abbr.edit')
+    @features.command('abbr.edit')
     def edit_command(self, event):
         if len(event.data) < 2:
             if len(event.data) < 1:
                 event.error("I can't edit nothing!")
             else:
-                event.error("You need to define a new expansion for '%s'" % event.data[0])
+                event.error("You need to define a new expansion for '{}'".format(event.data[0]))
 
             event.reply(self.USAGE_EDIT, True)
             return
 
         abbreviation = event.data[0]
-        if abbreviation not in self.abbrs:
-            event.error("I don't have an abbreviation for '%s'.  Try the add command instead." % abbreviation)
+        expansion = event.data[1]
+        current = self.db.abbrs.find_one({"abbreviation": abbreviation})
+
+        if current is None:
+            event.error("I don't have an abbreviation for '{}'.  Try the abbr.add command instead.".format(abbreviation))
             event.reply(self.USAGE_ADD, True)
         else:
-            prev = self.abbrs[abbreviation]
-            self.abbrs[abbreviation] = event.data[1]
-            event.reply("Changed abbreviation for '%s' from '%s' to '%s'." % (abbreviation, prev, event.data[1]))
+            self.db.abbrs.update({"abbreviation": abbreviation}, {"$set": {"expansion": expansion}})
+            event.reply("Changed abbreviation for '{}' from '{}' to '{}'.".format(abbreviation, current["expansion"], expansion))
 
-    @command('abbr')
+    @features.command('abbr')
     def abbr_command(self, event):
         if len(event.data) < 1:
             event.error("You need to give me an abbreviation to expand!")
-            event.reply(USAGE_ABBR, True)
+            event.reply(self.USAGE_ABBR, True)
             return
 
         abbreviation = event.data[0]
-        if abbreviation not in self.abbrs:
-            event.error("I don't have an abbreviation for '%s'.  Ask someone to !abbr.add one." % abbreviation)
-        else:
-            event.reply("'%s' = '%s'.  If this abbreviation is wrong, !abbr.edit it." % (abbreviation, self.abbrs[abbreviation]))
+        current = self.db.abbrs.find_one({"abbreviation": abbreviation})
 
-    @command('abbr.rem')
+        if current is None:
+            event.error("I don't have an abbreviation for '{}'.  Ask someone to abbr.add one.".format(abbreviation))
+        else:
+            event.reply("'{}' = '{}'.  If this abbreviation is wrong, abbr.edit it.".format(abbreviation, current["expansion"]))
+
+    @features.command('abbr.rem')
+    @features.command('abbr.del')
     def rem_command(self, event):
         if len(event.data) < 1:
             event.error("You need to give me an abbreviation to remove!")
-            event.reply(USAGE_REM, True)
+            event.reply(self.USAGE_REM, True)
             return
 
         abbreviation = event.data[0]
-        if abbreviation not in self.abbrs:
-            event.reply("'%s' was not in my list anyway." % abbreviation)
+        current = self.db.abbrs.find_one({"abbreviation": abbreviation})
+
+        if current is None:
+            event.reply("'{}' was not in my list anyway.".format(abbreviation))
         else:
-            prev = self.abbrs[abbreviation]
-            del self.abbrs[abbreviation]
-            event.reply("Removed the abbreviation '%s' = '%s'" % (abbreviation, prev))
+            self.db.abbrs.remove({"abbreviation": abbreviation})
+            event.reply("Removed the abbreviation '{}' = '{}'".format(abbreviation, current["expansion"]))
 
         
 
