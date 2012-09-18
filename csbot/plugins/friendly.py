8d75079432fc1974bc8726de8aab3130e4464362
Helen M. Gray <helen.m.gray@gmail.com>
2012-09-18 14:08:43 +0100

    Add initial 'todo' for friendly plugin.

---
+++ csbot/plugins/friendly.py
@@ -0,0 +1,28 @@
+# Friendly plugin
+
+# hugs
+#    !hug foo -> /me hugs foo
+#		or 'squeezes', 'squishes', 'embraces', 'cuddles' etc
+#    foo hugs Mathison -> /me hugs foo
+#		or '<3'
+
+# kiss
+#	foo kisses Mathison -> scandalised response
+#   !kiss foo -> /me kisses foo gently on the cheek
+#		multiple chaste responses
+
+# greeting
+# 	<foo> hello/hi/hey Mathison -> hi foo!
+#   foo joins, <foo> hi -> hi foo!
+#		has to be after join or similar to not get into a loop
+
+# 8-ball
+#	<foo> Mathison: something? -> an 8-ball-like response
+#		'consult Google', 'looks likely', 'as likely as <silly simile>'
+
+# dance
+#	!dance -> pick a random 3-state dance, e.g. \o/ \o_ _o_
+#		or do as 5-state and keep it to one line
+#		poss only allow 'valid' transitions, e.g. _ to \, \ to | but not _ to |
+#   foo dances with Mathison / !dance foo -> /me twirls foo
+#		or 'spins' etc
